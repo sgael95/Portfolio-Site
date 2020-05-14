1af0c7252bc589663d87d70ac816179cf7a37441
Gael Sanchez <sgael95@gmail.com>
2020-05-14 00:17:25 -0500

    github pages testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
+  output: {
+    publicPath: "/Porfolio-Site/",
+  },
   resolve: {
-      extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        loader: 'babel-loader'
+        loader: "babel-loader",
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
-          test: /\.(jpg|png)$/,
-          use: 'url-loader'
-      }
-    ]
+        test: /\.(jpg|png)$/,
+        use: "url-loader",
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
+      filename: "./index.html",
+    }),
+  ],
 };
